863ed29c52d089cdc87b575e0f1db35fedcbdbcf
Eduardo Unrrein <gum23coder@gmail.com>
2024-07-02 14:31:20 -0300

    Terminada entrega final

--- src/dao/db/db.js
+++ src/dao/db/db.js
@@ -1,18 +1,6 @@
 import mongoose from "mongoose";
 import config from "../../config.js";
 
-
-// (async () => {
-//   try {
-//     const db = await mongoose.connect(
-//       `mongodb+srv://${config.mongodb_user}:${config.mongodb_password}@${config.mongodb_cluster}/${config.mongodb_dbname}?retryWrites=true&w=majority`
-//     );
-//     console.log("Database is connected to: ", db.connection.name);
-//   } catch (error) {
-//     console.error("The connection to database is broken ", error);
-//   }
-// })();
-
 // const URI = `mongodb+srv://${config.mongodb_user}:${config.mongodb_password}@${config.mongodb_cluster}/${config.mongodb_dbname}?retryWrites=true&w=majority`;
 const URI = `${config.mongodb_uri}`;
 const db = async () => { 
